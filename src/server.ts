fa5ac9588f4da6d38fb13e462aeedb13cc6f4609
Rafi Ferdos <rafiferdos@gmail.com>
2026-05-18 17:16:31 +0600

    refactor: improve response structure in GET endpoint and format code

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
-import express, {type Application, type Request, type Response} from 'express'
+import express, { type Application, type Request, type Response } from 'express'
 const app: Application = express()
 const port = 5000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+  res.status(200).json({
+    message: 'server is ongoing',
+    creator: 'rafiferdos'
+  })
+})
+app.post('/', (req: Request, res: Response) => {
+  console.log(req)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
