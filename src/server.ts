9b3f692dbf526ef6f4c182262144df66d8e63fbe
Rafi Ferdos <rafiferdos@gmail.com>
2026-05-18 17:50:37 +0600

    feat: add PostgreSQL connection pool and update middleware configurations

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,12 @@ const app: Application = express()
 const port = 5000
 
 app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended: true}))
+
+const pool = new Pool({
+    connectionString: 'postgresql://neondb_owner:npg_aDxSsd3yr0et@ep-autumn-paper-apsyz174-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
