d98f6e33f378bf4eabae459fb860a9fc89b2a7d1
Rafi Ferdos <rafiferdos@gmail.com>
2026-05-18 17:23:14 +0600

    refactor: update request logging and ensure JSON parsing middleware is applied

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { type Application, type Request, type Response } from 'express'
 const app: Application = express()
 const port = 5000
 
+app.use(express.json())
+
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     message: 'server is ongoing',
@@ -9,7 +11,7 @@ app.get('/', (req: Request, res: Response) => {
   })
 })
 app.post('/', (req: Request, res: Response) => {
-  console.log(req)
+  console.log(req.body)
 })
 
 app.listen(port, () => {
