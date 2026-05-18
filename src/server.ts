16238fc596c75aebb7c207add304efd461e1603b
Rafi Ferdos <rafiferdos@gmail.com>
2026-05-18 16:51:18 +0600

    refactor: enhance type definitions in server.ts and tidy tsconfig.json

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from 'express'
-const app = express()
+import express, {type Application, type Request, type Response} from 'express'
+const app: Application = express()
 const port = 5000
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
