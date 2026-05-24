bef84e830e36aaf67e5251ea7d835548c2b664fb
Rafi Ferdos <rafiferdos@gmail.com>
2026-05-24 17:43:34 +0600

    feat: add logging middleware to app and create logs file for request tracking

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { type Application, type Request, type Response } from 'express'
+import fs from 'fs'
 import config from './config/index.js'
 import { authRoute } from './modules/auth/auth.route.js'
 import { profileRoute } from './modules/profile/profile.route.js'
@@ -10,6 +11,24 @@ const port = config.port
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true }))
+
+app.use((req, res, next) => {
+  console.log(
+    'method, url, time - ',
+    req.method,
+    ', ',
+    req.url,
+    ', ',
+    Date.now()
+  )
+  const log = `method -> ${req.method} | url -> ${req.url} | time -> ${Date.now()}`
+  fs.appendFile('logs.txt', log + '\n', (err) => {
+    if (err) {
+      console.error('Error writing to log file:', err)
+    }
+  })
+  next()
+})
 
 //* user route
 app.use('/api/users', userRoute)
