56a88619cd381715cb64015de9f5554532bae31a
Rafi Ferdos <rafiferdos@gmail.com>
2026-05-26 14:07:11 +0600

    feat: enhance authentication middleware with user validation and error handling

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,12 @@
 import { type NextFunction, type Request, type Response } from 'express'
+import jwt, { type JwtPayload } from 'jsonwebtoken'
+import config from '../config/index.js'
+import { pool } from '../db/index.js'
 
 const auth = () => {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization
+    try {
+          const token = req.headers.authorization
 
     if (!token) {
       res.status(401).json({
@@ -11,8 +15,33 @@ const auth = () => {
       })
       return
     }
-    // console.log(token)
-    next()
+
+    const decoded = jwt.verify(
+      token as string,
+      config.jwt_secret as string
+    ) as JwtPayload
+
+    const userData = pool.query(`
+        SELECT * FROM users WHERE email=$1
+      `, [decoded.email])
+    
+    if ((await userData).rows.length === 0) return res.status(404).json({
+      success: false,
+      message: 'user not found'
+    })
+
+    const user = (await userData).rows[0]
+    if (!user.is_active) return res.status(403).json({
+      success: false,
+      message: 'forbidden'
+    })
+
+    req.user = decoded
+    
+    next()  
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
